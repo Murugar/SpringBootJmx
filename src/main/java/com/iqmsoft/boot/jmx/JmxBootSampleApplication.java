4862ebb40e1f12f843093389beb6bb53c8ad4d6d
Davanon <davanon2014@gmail.com>
2017-06-01 15:52:30 +0530

    first

---
+++ src/main/java/com/iqmsoft/boot/jmx/JmxBootSampleApplication.java
@@ -0,0 +1,11 @@
+package com.iqmsoft.boot.jmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxBootSampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JmxBootSampleApplication.class, args);
+	}
+}
